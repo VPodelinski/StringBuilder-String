fff6cf1d7eeb4664704e3f2a432e9347d504f679
Vitali Podelinski <viivpo2014@gmail.com>
2017-06-23 15:25:21 +0300

    git merge origin/dev!

---
+++ src/mod/WithString.java
@@ -0,0 +1,24 @@
+package mod;
+
+/**
+ * Created by viivpo2010 on 19.6.17.
+ */
+public  class WithString {
+
+    public static String addWithString(String s, int count){
+        long t = System.currentTimeMillis();
+
+        for(int i = 1; i <=count; i++){
+
+
+            s+= " word"+i +" ";
+
+        }
+
+        System.out.println("Total time String (sec): " + ((System.currentTimeMillis() - t) / 1000));
+        return s;
+    }
+
+
+}
+
